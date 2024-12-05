ae8784e3a0e2ecd09ae6e36272005cb8e95a1d1b
Will Gorman <will.gorman@joyent.com>
2024-12-05 10:24:52 -0600

    add a dagger service

---
+++ main.go
@@ -0,0 +1,48 @@
+// A generated module for SampleDaggerModule functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+
+	"dagger/sample-dagger-module/internal/dagger"
+)
+
+type SampleDaggerModule struct{}
+
+// Returns a container that echoes whatever string argument is provided
+func (m *SampleDaggerModule) ContainerEcho(stringArg string) *dagger.Container {
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+}
+
+// Returns lines that match a pattern in the files of the provided Directory
+func (m *SampleDaggerModule) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
+	return dag.Container().
+		From("alpine:latest").
+		WithMountedDirectory("/mnt", directoryArg).
+		WithWorkdir("/mnt").
+		WithExec([]string{"grep", "-R", pattern, "."}).
+		Stdout(ctx)
+}
+
+func (m *SampleDaggerModule) HttpService() *dagger.Service {
+	return dag.Container().
+		From("python").
+		WithWorkdir("/srv").
+		WithNewFile("index.html", "Hello, world!").
+		WithExec([]string{"python", "-m", "http.server", "8080"}).
+		WithExposedPort(8080).
+		AsService()
+}
